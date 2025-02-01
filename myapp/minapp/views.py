0d7a1347754a76127e93c66bba9c9dcb35226442
Busami Victor <victorbusami1@gmail.com>
2025-02-01 12:54:38 +0300

    wordcounter commit

--- myapp/minapp/views.py
+++ myapp/minapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    context = {
+        'name': 'victor',
+        'age': 20,
+        'nationality': 'kenyan'
+    }
+    return render(request, 'index.html', context)
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html',{'amount':amount_of_words})
